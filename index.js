e79fd64647f84bbda5f34cdbf439f74bba5842f3
DESKTOP-RQ0PIPT\agrig <jasonrust89@gmail.com>
2017-10-05 13:36:23 +0300

    init express app;

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
 let session = require('express-session');
-let passport = require('passport');
+require('env/loader').setEnv(require('./.env'));
+let env = require('env');
 
+app.listen(env('port', 8080), () => {
+    console.log(`Server listening on http://localhost:${env('port', 8080)}/ !`);
+});
 
 module.exports = app;
